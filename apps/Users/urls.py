579b2c71655faab7411a1aa4d61d1c7f2d20173c
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-20 14:43:25 +0300

    Added OCS & Officer Register Page

--- apps/Users/urls.py
+++ apps/Users/urls.py
@@ -4,5 +4,7 @@ from apps.Users import views
 
 urlpatterns = [
     path('register/get-started', views.GetStartedRegister, name="GetStartedRegister"),
-    path('login/get-started', views.GetStartedLogin, name="GetStartedLogin")
+    path('login/get-started', views.GetStartedLogin, name="GetStartedLogin"),
+    path('ocs/register', views.OCSRegister, name="OCSRegister"),
+    path('officer/register', views.OfficerRegister, name="OfficerRegister")
 ]
--- apps/Users/views.py
+++ apps/Users/views.py
@@ -6,3 +6,9 @@ def GetStartedRegister(request):
 
 def GetStartedLogin(request):
     return render(request, 'Get Started Login.html')
+
+def OCSRegister(request):
+    return render(request, 'OCS Register.html')
+
+def OfficerRegister(request):
+    return render(request, 'Police Officer Register.html')
