d7bcfb029823b34338a94eb0a6ef2a9aeb888863
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-21 18:39:49 +0300

    Added Accused Person Model

--- apps/Accused/models.py
+++ apps/Accused/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+GENDER = [
+    ('Male', ('Male')),
+    ('Female', ('Female')),
+]
+
+ARREST_STATUS = [
+    ('Free', ('Free')),
+    ('In Cutody', ('In Cutody')),
+    ('Out On Bond', ('Out On Bond')),
+    ('Out On Cash Bail', ('Out On Cash Bail')),
+]
 
 # Create your models here.
+class AccusedPerson(models.Model):
+    arrest_number = models.CharField(max_length=255, verbose_name='Arrest Number.')
+    first_name = models.CharField(max_length=100, verbose_name='First Name')
+    middle_name = models.CharField(max_length=100, verbose_name='Middle Name')
+    last_name = models.CharField(max_length=100, verbose_name='Last Name')
+    date_of_birth = models.DateField(verbose_name='Date Of Birth')
+    gender = models.CharField(choices=GENDER, max_length=10, verbose_name='Gender')
+    national_id = models.CharField(max_length=10, verbose_name='National ID')
+    # profile_picture = CloudinaryField('accused_persons_profile_picture')
+    profile_picture = models.ImageField(upload_to='Accused-Profile-Pics', verbose_name='Accused Person Profile Pics')
+    arrest_status = models.CharField(choices=ARREST_STATUS, max_length=50, verbose_name='Arrest Status')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Date Created')
+    date_updated = models.DateTimeField(auto_now=True, verbose_name='Date Updated')
+    
+    def __str__(self):
+        return str(self.first_name + ' ' + self.middle_name + ' ' + self.last_name)
+    
+    class Meta:
+        verbose_name_plural = 'Accused Persons Profiles'
