265c0183e80971130a56c962c783089e2ab2441f
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-24 02:56:30 +0300

    Made changes to edit court details function

--- apps/Courts/views.py
+++ apps/Courts/views.py
@@ -43,28 +43,34 @@ def EditCourtInfo(request, id):
     court = Court.objects.get(id=id)
 
     if request.method == 'POST':
-        context = {'has_error': False}
-        court_number = request.POST['court_number']
-        accused_person = request.POST['accused_person']
-        court = request.POST['court']
-        court_verdict = request.POST['court_verdict']
-        scheduled_on = request.POST['scheduled_on']
+        form = EditCourtInfoForm(request.POST)
 
-        court.court_number = court_number
-        court.accused_person = AccusedPerson.objects.get(pk=int(accused_person))
-        court.court = court
-        court.court_verdict = court_verdict
-        court.scheduled_on = scheduled_on
-        court.created_by = request.user.profile
+        if form.is_valid():
+            context = {'has_error': False}
+            court_number = form.cleaned_data['court_number']
+            accused_person = form.cleaned_data['accused_person']
+            court = form.cleaned_data['court']
+            court_verdict = form.cleaned_data['court_verdict']
+            scheduled_on = form.cleaned_data['scheduled_on']
 
-        if not context['has_error']:
-            court.save()
-            messages.success(request, '✅ Court Record Successfully Updated!')
-            return redirect('OfficerCourtInfo')
-                
+            court.court_number = court_number
+            court.accused_person = AccusedPerson.objects.get(pk=int(accused_person))
+            court.court = court
+            court.court_verdict = court_verdict
+            court.scheduled_on = scheduled_on
+            court.created_by = request.user.profile
+
+            if not context['has_error']:
+                court.save()
+                messages.success(request, '✅ Court Record Successfully Updated!')
+                return redirect('OfficerCourtInfo')
+                    
+            else:
+                messages.error(request, '⚠️ Court Record Was Not Updated!')
+                return redirect('OfficerCourtInfo')
+    
         else:
-            messages.error(request, '⚠️ Court Record Was Not Updated!')
-            return redirect('OfficerCourtInfo')
+            form = EditCourtInfoForm(instance=request.user.profile)
 
     return redirect('OfficerCourtInfo')
 
